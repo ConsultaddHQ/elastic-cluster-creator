1ace09cac115a9d40ea43f18fbea01448c7400bf
namanagrawal22 <naman.a@consultadd.com>
2025-06-24 10:14:08 -0400

    changes

--- main.py
+++ main.py
@@ -147,12 +147,12 @@ def main():
     print("✅ Terraform apply completed and outputs saved to terraform_output.json")
 
     print("Running Ansible playbook to configure Elasticsearch cluster")
+    os.chdir("../ansible-role")
+
     if cloud == "aws":
-        run_command("cd ../ansible-role")
         run_command("ansible-playbook -i  ../aws/inventory.yaml playbook.yaml")
     
     elif cloud == "gcp":
-        run_command("cd ../ansible-role")
         run_command("ansible-playbook -i  ../gcp/inventory.yaml playbook.yaml")
 
 
