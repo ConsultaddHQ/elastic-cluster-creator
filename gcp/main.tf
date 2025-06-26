a4a5965301fd8b47fba357df40d7cc959e662f20
vijay patidar <vramotiya@gmail.com>
2025-06-26 17:27:05 +0530

    Create and attach service account to ce

--- gcp/main.tf
+++ gcp/main.tf
@@ -4,18 +4,26 @@ provider "google" {
   credentials = file(var.gcp_credentials_file)
 }
 
+# Create Service Account
+resource "google_service_account" "elk_sa" {
+  account_id   = "${var.cluster_name}-service-account"
+  display_name = "${var.cluster_name} Service Account"
+}
+
+# Firewall Rule
 resource "google_compute_firewall" "elastic_fw" {
   name    = "elastic-cluster-fw"
   network = "default"
 
   allow {
     protocol = "tcp"
-    ports    = ["22", "9200", "5601","9300"]
+    ports    = ["22", "9200", "5601", "9300"]
   }
 
   source_ranges = ["0.0.0.0/0"]
 }
 
+# Master Node
 resource "google_compute_instance" "es_master" {
   name         = "${var.cluster_name}-master-node"
   machine_type = var.instance_type
@@ -37,6 +45,11 @@ resource "google_compute_instance" "es_master" {
   }
 
   tags = ["elasticsearch"]
+
+  service_account {
+    email  = google_service_account.elk_sa.email
+    scopes = ["cloud-platform"]
+  }
 }
 
 resource "google_compute_instance" "es_kibana" {
@@ -60,8 +73,14 @@ resource "google_compute_instance" "es_kibana" {
   }
 
   tags = ["elasticsearch"]
+
+  service_account {
+    email  = google_service_account.elk_sa.email
+    scopes = ["cloud-platform"]
+  }
 }
 
+# Data Nodes
 resource "google_compute_instance" "es_data" {
   count        = var.data_count
   name         = "${var.cluster_name}-data-node-${count.index + 1}"
@@ -84,8 +103,14 @@ resource "google_compute_instance" "es_data" {
   }
 
   tags = ["elasticsearch"]
+
+  service_account {
+    email  = google_service_account.elk_sa.email
+    scopes = ["cloud-platform"]
+  }
 }
 
+# Master-Eligible Nodes
 resource "google_compute_instance" "es_master_eligible" {
   count        = var.master_eligible
   name         = "${var.cluster_name}-master-eligible-node-${count.index + 1}"
@@ -108,5 +133,9 @@ resource "google_compute_instance" "es_master_eligible" {
   }
 
   tags = ["elasticsearch"]
-}
 
+  service_account {
+    email  = google_service_account.elk_sa.email
+    scopes = ["cloud-platform"]
+  }
+}
